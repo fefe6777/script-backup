afe3a6c982f4250a4eca0d5dd8191cc9fe513741
FELIPE ALVES LIMA <felipe.alima26@senacsp.edu.br>
2024-10-21 16:20:54 -0300

    Versao 1.0

---
+++ backup.bat
@@ -0,0 +1,5 @@
+mkdir BACKUP
+copy \Windows\*.txt \Users\felipe.alima26\Documents\BACKUP
+copy \Windows\System32\*.txt \Users\felipe.alima26\Downloads\BACKUP
+copy \windows\System32\*.txt \Users\felipe.alima26\Downloads\BACKUP
+ 
